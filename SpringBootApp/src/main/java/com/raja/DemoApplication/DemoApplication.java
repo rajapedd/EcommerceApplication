49e39792cc534173e39403b368996fe73e3eeb5f
rajapedd <rajapeddanna.kadavakallu@gmail.com>
2023-10-13 15:22:54 +0530

    Pushing services

--- SpringBootApp/src/main/java/com/raja/DemoApplication/DemoApplication.java
+++ SpringBootApp/src/main/java/com/raja/DemoApplication/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Test Live Running....");
+		System.out.println("Spring Boot Demo App is Running.....!!!");
 	}
 
 }
